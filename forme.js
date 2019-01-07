ed1e2ab6d9e46d913e0db06da365f89cd196df6d
ysabel06 <45562815+ysabel06@users.noreply.github.com>
2019-01-07 15:42:46 +0100

    Add files via upload

---
+++ forme.js
@@ -0,0 +1,140 @@
+ $(function(){
+
+ 	$("#plein").click(function(){
+ 		$(".carré1,.rond1,.rond2,.rond3,.rond4,.rond5,.carré2,.rond6,.rond7,.rond8,.rond9,.rond10,.rond11,.losange1,.rond12,.rond13,.rond14,.rond15,.carré3,.rond16,.rond17,.rond18,.rond19,.rond20,.carré4").addClass("red size border");
+
+ 	});
+
+ 	$("#vide").click(function(){
+ 		$(".carré1,.rond1,.rond2,.rond3,.rond4,.carré2,.rond5,.rond6,.rond7,.rond8,.rond9,.rond10,.rond11,.losange1,.rond12,.rond13,.rond14,.rond15,.carré3,.rond16,.rond17,.rond18,.rond19,.rond20,.carré4").removeClass("red size border");
+
+
+ 	});
+
+ 	$(".carré1").click(function(){
+ 		$(".carré1").toggleClass("blue");
+
+	});
+	$(".rond1").click(function(){
+ 		$(".rond1").toggleClass("red");
+ 	});
+
+ 	$(".rond2").click(function(){
+ 		$(".rond2").toggleClass("red");
+ 	});
+ 	$(".rond3").click(function(){
+ 		$(".rond3").toggleClass("red");
+ 	});
+
+ 	$(".rond4").click(function(){
+ 		$(".rond4").toggleClass("red");
+ 	});
+
+ 	$(".rond5").click(function(){
+ 		$(".rond5").toggleClass("red");
+ 	});
+
+
+ 	$(".carré2").click(function(){
+ 		$(".carré2,.carré2").toggleClass("blue");
+ 	});
+
+ 	$(".rond6").click(function(){
+ 		$(".rond6,.rond6").toggleClass("red");
+ 	});
+
+ 	$(".rond7").click(function(){
+ 		$(".rond7,.rond7").toggleClass("red");
+ 	});
+
+ 	$(".rond8").click(function(){
+ 		$(".rond8,.rond8").toggleClass("red");
+
+ 	});
+ 		$(".rond9").click(function(){
+ 		$(".rond9,.rond0").toggleClass("red");
+ 	});
+ 	    	$(".rond10").click(function(){
+ 		$(".rond10,.rond10").toggleClass("red");
+ 	});
+
+ 	    	$(".rond11").click(function(){
+ 		$(".rond11,.rond11").toggleClass("red");
+
+ 	});
+ 	    	$(".losange1").click(function(){
+ 		$(".losange1,.losange1").toggleClass("green");
+
+ 	});
+ 	    	$(".rond12").click(function(){
+ 		$(".rond12,.rond1é").toggleClass("red");
+ 	});
+ 	    	$(".rond13").click(function(){
+ 		$(".rond13,.rond13").toggleClass("red");
+ 	});
+ 	    	$(".rond14").click(function(){
+ 		$(".rond14,.rond14").toggleClass("red");
+ 	});
+ 	    	$(".rond15").click(function(){
+ 		$(".rond15,.rond15").toggleClass("red");
+ 	});
+ 	    	$(".carré3").click(function(){
+ 		$(".carré3,.carré3").toggleClass("blue");
+ 	});
+ 	    	$(".rond16").click(function(){
+ 		$(".rond16,.rond16").toggleClass("red");
+
+ 	});
+ 	    	$(".rond17").click(function(){
+ 		$(".rond17,.rond17").toggleClass("red");
+ 	});
+ 	    	$(".rond18").click(function(){
+ 		$(".rond18,.rond18").toggleClass("red");
+ 	});
+ 	    	$(".rond19").click(function(){
+ 		$(".rond19,.rond19").toggleClass("red");
+ 	});
+
+           	$(".rond20").click(function(){
+ 		$(".rond20,.rond20").toggleClass("red");
+ 	});
+
+ 	    	$(".carré4").click(function(){
+ 		$(".carré4,.carré4").toggleClass("blue");
+ 	});
+
+
+
+
+
+
+
+
+
+
+
+
+	
+
+
+
+
+    });
+
+    
+
+ 	
+
+
+ 		
+ 	
+ 
+ 
+
+
+ 	
+   
+
+ 
+    
+   
